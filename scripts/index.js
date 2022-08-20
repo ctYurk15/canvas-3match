bdfc5f6386c03af82d7b2292132380b99a1cc6d2
ctYurk15 <levgenetic@gmail.com>
2022-08-20 16:07:28 +0300

    Add start & restart buttons

--- scripts/index.js
+++ scripts/index.js
@@ -5,16 +5,24 @@ let current_square_point = null;
 function start(engine, matrix, progress_tracker)
 {
     matrix.regenerateArray(square_width, square_colors);
+    progress_tracker.restoreProgress();
+
     progress_tracker.startTimeUpdate();
+
     engine.start();
 }
 
 //ui-elements
 const scores_text = document.querySelector('#scoresText');
 const time_text = document.querySelector('#timeText');
-const end_time_modal = document.querySelector('#endTimeModal');
 const end_time_scores_text = document.querySelector('#endTimeScoresText');
 const highscores_text = document.querySelector('#highScoresText');
+
+const end_time_modal = document.querySelector('#endTimeModal');
+const menu_modal = document.querySelector("#menuModal");
+
+const start_game_button = document.querySelector('#startGameButton');
+const restart_game_button = document.querySelector('#restartGameButton');
 
 //configuring canvas
 const canvas = document.querySelector('canvas');
@@ -94,14 +102,24 @@ window.addEventListener('click', function(event){
 
 });
 
-//and of the game
+//end of the game
 progress_tracker.setEndTimeAction(function(){
     engine.is_working = false;
     end_time_modal.classList.remove('hidden');
     endTimeScoresText.innerHTML = 'Scores: '+progress_tracker.scores;
 });
 
-start(engine, matrix, progress_tracker);
+start_game_button.addEventListener('click', function(){
+    start(engine, matrix, progress_tracker);
+    menu_modal.classList.add('hidden');
+});
+
+restart_game_button.addEventListener('click', function(){
+    start(engine, matrix, progress_tracker);
+    end_time_modal.classList.add('hidden');
+});
+
+engine.clear();
 
 //game loop
 let animation_frame;
--- scripts/progress.js
+++ scripts/progress.js
@@ -1,24 +1,34 @@
 class Progress
 {
     scores = 0;
+    total_seconds = 0;
     end_time_action = null;
 
     constructor(scores_text_container, time_text_container, total_seconds)
     {
         this.scores_text_container = scores_text_container;
         this.time_text_container = time_text_container;
+        this.current_seconds = total_seconds;
         this.total_seconds = total_seconds;
+    }
+
+    restoreProgress()
+    {
+        this.current_seconds = this.total_seconds;
+        this.scores = 0;
+        this.updateUI();
     }
 
     addScores(scores)
     {
         this.scores += scores;
-        this.updateScoresText();
+        this.updateUI();
     }
 
-    updateScoresText()
+    updateUI()
     {
         this.scores_text_container.innerHTML = 'Scores: '+this.scores;
+        this.time_text_container.innerHTML = 'Time left: '+this.current_seconds+'s';
     }
 
     startTimeUpdate()
@@ -27,15 +37,15 @@ class Progress
 
         let interval = setInterval(function(){
 
-            if(self.total_seconds <= 0)
+            if(self.current_seconds <= 0)
             {
                 clearInterval(interval);
                 self.end_time_action();
             }
             else
             {
-                self.total_seconds--;
-                self.time_text_container.innerHTML = 'Time left: '+self.total_seconds+'s';
+                self.current_seconds--;
+                self.time_text_container.innerHTML = 'Time left: '+self.current_seconds+'s';
             }
 
 
